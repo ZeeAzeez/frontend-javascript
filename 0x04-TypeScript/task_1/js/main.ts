0d127f35ad07a9d1cb58d460ae8c1760dc7d1a1a
ZeeAzeez <azeezahaazeez@gmail.com>
2025-11-09 23:56:44 +0100

    function printTEACHER

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,6 +11,14 @@ interface Director extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -29,3 +37,5 @@ const director1: Director = {
 
 console.log(director1);
 console.log(teacher3);
+
+console.log(printTeacher("John", "Doe"));
