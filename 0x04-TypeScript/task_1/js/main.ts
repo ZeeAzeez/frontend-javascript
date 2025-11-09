42957eb14a3e47990c576eb10ba4b899af9ef9a9
ZeeAzeez <azeezahaazeez@gmail.com>
2025-11-10 00:00:15 +0100

    successfuly passign checkers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,9 +15,9 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
+function printTeacher({ firstName, lastName }: { firstName: string, lastName: string }): string {
+  return `${firstName}. ${lastName}`;
+}
 
 const teacher3: Teacher = {
   firstName: 'John',
@@ -38,4 +38,4 @@ const director1: Director = {
 console.log(director1);
 console.log(teacher3);
 
-console.log(printTeacher("John", "Doe"));
+console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
