4fe40b802e2b9966ca747eb08babc396f024bf49
ZeeAzeez <azeezahaazeez@gmail.com>
2025-11-10 17:40:19 +0100

    task 5

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -34,6 +34,49 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+interface DirectorInterface{
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface{
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface{
+  workFromHome(): string {
+    return "Working from home";
+  }
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+  workDirectorTasks(): string{
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface{
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workTeacherTasks(): string{
+    return "Getting to work"
+  }
+}
+function createEmployee(salary: number | string): DirectorInterface | TeacherInterface {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
 function printTeacher({ firstName, lastName }: { firstName: string, lastName: string }): string {
   return `${firstName}. ${lastName}`;
 }
@@ -45,4 +88,9 @@ console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
 
 const student1 = new Student("Azeezah", "Abdulazeez");
 console.log(student1.displayName());     
-console.log(student1.workOnHomework());   
+console.log(student1.workOnHomework());
+
+// Test createEmployee function
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));   
