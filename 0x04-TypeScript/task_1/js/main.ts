9566513f9133a82f8c2cf96358aa52b2e002e8dd
ZeeAzeez <azeezahaazeez@gmail.com>
2025-11-10 13:39:10 +0100

    removed [0]  in task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,7 +35,7 @@ interface printTeacherFunction {
 }
 
 function printTeacher({ firstName, lastName }: { firstName: string, lastName: string }): string {
-  return `${firstName[0]}. ${lastName}`;
+  return `${firstName}. ${lastName}`;
 }
 const Student: StudentConstructor = StudentClass;
 
