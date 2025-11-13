28303ae86a10354087b67964d6ecbe7fccde95d6
ZeeAzeez <azeezahaazeez@gmail.com>
2025-11-14 00:20:43 +0100

    task 11

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,26 @@
+// Branded interfaces for type safety
+interface MajorCredits {
+    credits: number;
+    _brand: "major";
+}
+
+interface MinorCredits {
+    credits: number;
+    _brand: "minor";
+}
+
+// Function to sum MajorCredits
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        _brand: "major"
+    };
+}
+
+// Function to sum MinorCredits  
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        _brand: "minor"
+    };
+}
